93039ed720577f90f928de57286c6299f93861ce
Jean Paul Saucedo <oymdev1@oym.com.py>
2018-03-05 05:17:53 -0300

    Agregando put y delete

--- src/services/rest-api.service.ts
+++ src/services/rest-api.service.ts
@@ -3,16 +3,26 @@ import { Injectable } from '@angular/core';
 
 Injectable()
 export class RestApiService {
+
+  url: string = "https://example/api/v1";
   constructor(private http: HttpClient) {
 
   }
 
   get(link: string) {
-    return this.http.get(link).toPromise();
+    return this.http.get(this.url + '/' + link).toPromise();
   }
 
   post(link: string, body: any) {
-    return this.http.post(link, body).toPromise();
+    return this.http.post(this.url + '/' + link, body).toPromise();
+  }
+
+  put(link: string, body: any) {
+    return this.http.put(this.url + '/' + link, body).toPromise();
+  }
+
+  delete(link: string, body: any) {
+    return this.http.delete(this.url + '/' + link, body).toPromise();
   }
 
 }
